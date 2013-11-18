734a951553e6e57d340a4f03f370637597127305
David Murray <cykey@live.com>
2013-11-18 02:27:38 +0000

    Logging.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 #include <errno.h>
 #include <unistd.h>
 #include <signal.h>
@@ -13,6 +14,7 @@
 enum deg_str_type { deg_dd, deg_ddmm, deg_ddmmss };
 
 static struct gps_data_t gpsdata;
+static FILE *log_file;
 
 static void get_google_maps_image_from_coordinates(double latitude, double longitude, int zoom_level, int width, int height, const char *out_file);
 static void die(int reason);
@@ -66,6 +68,9 @@ static void die(int reason)
 	/* Close the connection to gpsd. */
 	gps_close(&gpsdata);
 
+	/* Close the log file. */
+	fclose(log_file);
+
 	fprintf(stderr, "Exiting: %d\n", reason);
 
 	exit(reason);
@@ -87,6 +92,24 @@ int main(int argc, char *argv[])
 
 	gps_stream(&gpsdata, WATCH_ENABLE, NULL);
 
+	/* Create an appropriate file name using from the current date & time. */
+	time_t timestamp = time(NULL);
+	struct tm local_time = *localtime(&timestamp);
+
+	char path[500];
+	sprintf(path, "log-%d-%d-%d-%d:%d:%d.txt", local_time.tm_year + 1900,
+						local_time.tm_mon + 1,
+						local_time.tm_mday,
+						local_time.tm_hour,
+						local_time.tm_min,
+						local_time.tm_sec);
+	/* Open the log file. */
+	log_file = fopen(path, "w");
+	if (!log_file) {
+		fprintf(stderr, "Couldn't open file at path: %s", path);
+		die(-1);
+	}
+
 	while (1) {
 		if (!gps_waiting(&gpsdata, 500000)) {
 			fprintf(stderr, "Timed out.\n");
@@ -101,14 +124,17 @@ int main(int argc, char *argv[])
 				if (fix_mode == MODE_NO_FIX || fix_mode == MODE_NOT_SEEN) {
 					fprintf(stderr, "No fix.\n");
 				} else {
-					double longitude = gpsdata.fix.longitude;
 					double latitude = gpsdata.fix.latitude;
+					double longitude = gpsdata.fix.longitude;
 
 					fprintf(stdout, "Fix mode: %d\n\t Lat: %f %c\n\t Long: %f %c\n", fix_mode, latitude,
 											  (latitude < 0) ? 'S' : 'N', longitude, (longitude < 0) ? 'W' : 'E');
 
 					/* Get the Google Static map. */
 					get_google_maps_image_from_coordinates(latitude, longitude, 17, 320, 240, "map.png");
+
+					/* Write data to the log file. */
+					fprintf(log_file, "%f,%f,%f,%f,%f\n", latitude, longitude, gpsdata.fix.altitude, gpsdata.fix.speed, gpsdata.fix.climb);
 
 					/* Not using this right now because it is broken. */
 //					fprintf(stdout, "Fix mode: %d\n\t Lat: %s %c\n\t Long: %s %c\n", fix_mode, deg_to_str(deg_dd, fabs(latitude)),
