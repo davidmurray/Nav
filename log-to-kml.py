133a4c0d6cb1ca11ed23a7e1f2519028222469e8
David Murray <cykey@live.com>
2013-11-18 18:23:16 -0500

    log-to-kml.py

---
+++ log-to-kml.py
@@ -0,0 +1,20 @@
+from simplekml import Kml
+import sys
+
+in_file, out_file = sys.argv[1], sys.argv[2];
+data = list()
+
+kml = Kml(name = in_file, open = 1)
+
+with open(in_file) as file:
+	lines = file.readlines()
+	for x in lines:
+		elements = x.split(',')[:3]
+		group = tuple(elements)
+		data.append(group)
+
+path = kml.newlinestring(name = "Flight", description = in_file, coords = data)
+path.coords = data
+
+kml.save(out_file)
+
